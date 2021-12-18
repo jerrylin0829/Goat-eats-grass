c27e8f7e54c0c837446b40a36b853e05d15a7622
s109703023 <80041556+s109703023@users.noreply.github.com>
2021-12-19 00:36:25 +0800

    Update Grass.h

--- Grass.h
+++ Grass.h
@@ -19,8 +19,22 @@ class Grass : public Creature
                 //Grass breed small grass
                 void breed(Creature *[20][35], int, int);
 
-        private:
+                //creature pass one time
+                void act(Creature *[20][35], int, int);
 
+                //creature is Acted
+                void setIsActed(bool);
+
+                //get isActed
+                bool getIsActed();
+
+                //creature death or not
+                bool die();
+
+                void move(Creature *[20][35], int, int);
+
+        private:
+                bool is_acted;
 };
 
 #endif
