0f2ab779bde3a1ea14978037709acff8c53992f1
s109703023 <80041556+s109703023@users.noreply.github.com>
2021-12-19 00:36:41 +0800

    Update Grass.cc

--- Grass.cc
+++ Grass.cc
@@ -8,7 +8,7 @@ using namespace std;
 //Constructor
 Grass::Grass() : Creature('I')
 {
-
+        is_acted = false;
 }
 //Desturctor
 Grass::~Grass()
@@ -17,7 +17,7 @@ Grass::~Grass()
 }
 
 void
-Goat::breed(Creature *cell[20][35], int y, int x)
+Grass::breed(Creature *cell[20][35], int y, int x)
 {
         int num = Creature::getRandomNumber();
         int new_y = y + dy[num];
@@ -26,4 +26,43 @@ Goat::breed(Creature *cell[20][35], int y, int x)
                 cell[new_y][new_x] = new Grass();
         }
 
+}
+
+void
+Grass::act(Creature *cell[20][35],int y, int x)
+{
+        if(cell[y][x]->getIsActed())
+                return;
+
+        cell[y][x]->Creature::increaseAge();
+        if(cell[y][x]->getAge() >= 3 && cell[y][x]->getAge() <= 5){
+                cell[y][x]->breed(cell, y, x);
+        }
+
+        cell[y][x]->setIsActed(true);
+}
+
+void
+Grass::setIsActed(bool _is_act)
+{
+        is_acted = _is_act;
+}
+
+bool
+Grass::getIsActed()
+{
+        return is_acted;
+}
+
+bool
+Grass::die()
+{
+        return Creature::getAge() > grassMaxAge;
+}
+
+//Grass can't move, but this is virtual function, so I have to do it.
+void
+Grass::move(Creature *cell[20][35], int y, int x)
+{
+        //return;
 }
