01e6020fc1f79ee85290970da9a4ae67fe01517c
s109703023 <80041556+s109703023@users.noreply.github.com>
2021-12-19 20:45:00 +0800

    Update Grass.cc

--- Grass.cc
+++ Grass.cc
@@ -1,68 +1,98 @@
 #include <bits/stdc++.h>
-#include "Grass.h"
-
-const int grassMaxAge = 6;
-
+#include "Goat.h"
+#include "Creature.h"
 using namespace std;
 
+const int goatMaxAge = 70;
+
 //Constructor
-Grass::Grass() : Creature('I')
+Goat::Goat() : Creature('X')
 {
+        foodPoints = 20;
         is_acted = false;
 }
-//Desturctor
-Grass::~Grass()
+
+//Destructor
+Goat::~Goat()
 {
 
 }
 
 void
-Grass::breed(Creature *cell[20][35], int y, int x)
+Goat::breed(Creature *cell[20][35], int y, int x)
 {
         int num = Creature::getRandomNumber();
         int new_y = y + dy[num];
         int new_x = x + dx[num];
-        if(cell[new_y][new_x]->getSign() == NULL){
-                cell[new_y][new_x] = new Grass();
-        }
 
+        if((new_y < 0 || new_y >= 20) || (new_x < 0 || new_x >= 35)){
+                cout << "goat's breed exceed bound" << endl;
+                return;
+        }
+        if(cell[new_y][new_x]->getSign() != 'X'){
+
+                if(cell[new_y][new_x]->getSign() == 'I'){
+                        foodPoints+=5;
+                        delete cell[new_y][new_x];
+                }
+
+                cell[new_y][new_x] = new Goat();
+        }
 }
 
 void
-Grass::act(Creature *cell[20][35],int y, int x)
+Goat::move(Creature *cell[20][35], int y, int x)
+{
+        int num = Creature::getRandomNumber();
+        int new_y = y + dy[num];
+        int new_x = x + dx[num];
+
+        if((new_y < 0 || new_y >= 20) || (new_x < 0 || new_x >= 35)){
+                cout << "goat's move exceed bound" << endl;
+                return;
+        }
+        if(cell[new_y][new_x] == NULL){
+                cell[new_y][new_x] = cell[y][x];
+        }
+        else if(cell[new_y][new_x]->getSign() == 'I'){
+                cell[new_y][new_x] = cell[y][x];
+                foodPoints+=5;
+        }
+
+        cell[y][x] = NULL;
+}
+void
+Goat::act(Creature *cell[20][35], int y, int x)
 {
         if(cell[y][x]->getIsActed())
                 return;
 
         cell[y][x]->Creature::increaseAge();
-        if(cell[y][x]->getAge() >= 3 && cell[y][x]->getAge() <= 5){
-                cell[y][x]->breed(cell, y, x);
-        }
-
         cell[y][x]->setIsActed(true);
+        foodPoints--;
+
+        if(cell[y][x]-> Creature::getAge() >= 55 && cell[y][x]-> Creature::getAge() <= 60){
+                cell[y][x]->breed(cell, y, x);
+        }else{
+                cell[y][x]->move(cell, y, x);
+                cout << "hi" << endl;
+        }
 }
 
 void
-Grass::setIsActed(bool _is_act)
+Goat::setIsActed(bool _is_act)
 {
         is_acted = _is_act;
 }
 
 bool
-Grass::getIsActed()
+Goat::getIsActed()
 {
         return is_acted;
 }
 
 bool
-Grass::die()
+Goat::die()
 {
-        return Creature::getAge() > grassMaxAge;
-}
-
-//Grass can't move, but this is virtual function, so I have to do it.
-void
-Grass::move(Creature *cell[20][35], int y, int x)
-{
-        //return;
+  return Creature::getAge() > goatMaxAge || foodPoints == 0;
 }
